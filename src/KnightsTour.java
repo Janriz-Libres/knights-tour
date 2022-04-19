2782c44c0861e60bacfb881479c5a0d4faf2d177
clintgalendez <clintgalendez@gmail.com>
2022-04-19 14:39:11 +0800

    added JFrame

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -1,8 +1,13 @@
 package src;
+import src.KnightsTourGUI;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 
 public class KnightsTour {
-    
+
+    public static void main(String[] args) {
+        KnightsTourGUI window = new KnightsTourGUI();
+        window.frame.setVisible(true);
+    }
 }
--- src/KnightsTourGUI.java
+++ src/KnightsTourGUI.java
@@ -4,13 +4,15 @@ import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 
 public class KnightsTourGUI extends JFrame {
-    
-    public void KnightsTourFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		JPanel contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+    JFrame frame;
+
+	public KnightsTourGUI() {
+		initialize();
+	}
+
+    private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
