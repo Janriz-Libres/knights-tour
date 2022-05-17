2af66b9a92b4bb98517a012f76e4de8aa782dc2e
ainune <104012102+ainune@users.noreply.github.com>
2022-05-17 12:50:03 +0800

    Update KnightsTourGUI.java

--- src/KnightsTourGUI.java
+++ src/KnightsTourGUI.java
@@ -33,6 +33,7 @@ public class KnightsTourGUI extends JFrame {
 	 */
 	public KnightsTourGUI() {
 		chessBoard = new JPanel(new GridLayout(BOARD_SIZE, BOARD_SIZE));
+		setTitle("Knight's Tour");
 		
 		JPanel controlPanel = new JPanel(new BorderLayout());
 		JPanel btnPanel = new JPanel();
@@ -40,8 +41,14 @@ public class KnightsTourGUI extends JFrame {
 		modeLabel = new JLabel("Mode: Auto", SwingConstants.CENTER);
 
 		JButton autoBtn = new JButton("Auto Tour");
+		autoBtn.setBackground(new Color(84,68,50));
+		autoBtn.setForeground(Color.WHITE);
 		JButton manualBtn = new JButton("Manual Tour");
+		manualBtn.setBackground(new Color(84,68,50));
+		manualBtn.setForeground(Color.WHITE);
 		JButton guidedBtn = new JButton("Guided Tour");
+		guidedBtn.setBackground(new Color(84,68,50));
+		guidedBtn.setForeground(Color.WHITE);
 
 		autoBtn.addActionListener(e -> sm.change(auto));
 		manualBtn.addActionListener(e -> sm.change(manual));
@@ -89,7 +96,7 @@ public class KnightsTourGUI extends JFrame {
 	 * @param btn a valid neighbor cell/button to be designed
 	 */
 	static void designNeighbor(JButton btn) {
-		btn.setBackground(new Color(180,160,126));
+		btn.setBackground(new Color(144,119,85,255));
 	}
 
 	/**
